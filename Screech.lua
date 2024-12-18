d834496773b7c358b85d0fd981e6423e6e2e0a1a
Tupo <denduhadelaetstrimy@gmail.com>
2024-12-18 18:01:50 +0300

    Create Screech.lua

---
+++ Screech.lua
@@ -0,0 +1,5 @@
+local screech game.ReplicatedStorage.Entities.Screech
+
+screech.Top.Eyes = Color3.fromRGB(255, 0, 0)
+screech.Top.Skin_Top = Color3.fromRGB(116, 94, 95)
+screech.Bottom.Skin_Bottom = Color3.fromRGB(116, 94, 95)
