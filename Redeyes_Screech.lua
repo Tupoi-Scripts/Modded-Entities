e1ad59b65ae0bbc41d7e4afe27c708d113dd1300
Tupo <denduhadelaetstrimy@gmail.com>
2024-12-18 19:06:00 +0300

    Update and rename Screech.lua to Redeyes_Screech.lua

---
+++ Redeyes_Screech.lua
@@ -0,0 +1,5 @@
+local screech game.ReplicatedStorage.Entities.Screech
+
+screech.Top.Eyes = Color.fromRGB(255, 0, 0)
+screech.Top.Skin_Top = Color.fromRGB(116, 94, 95)
+screech.Bottom.Skin_Bottom = Color.fromRGB(116, 94, 95)
--- Screech.lua
+++
@@ -1,5 +0,0 @@
-local screech game.ReplicatedStorage.Entities.Screech
-
-screech.Top.Eyes = Color3.fromRGB(255, 0, 0)
-screech.Top.Skin_Top = Color3.fromRGB(116, 94, 95)
-screech.Bottom.Skin_Bottom = Color3.fromRGB(116, 94, 95)
